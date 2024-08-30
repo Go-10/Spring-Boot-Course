78a909e7ef13a49ffaad52f46c20642ded554b6f
Go-10 <nagasivajisirigineedi.dev@gmail.com>
2024-08-30 11:51:40 +0530

    Creating bean using IOC container

--- SpringDemo/src/main/java/com/example/SpringDemo/Alien.java
+++ SpringDemo/src/main/java/com/example/SpringDemo/Alien.java
@@ -1,6 +1,16 @@
 package com.example.SpringDemo;
 
+import org.springframework.stereotype.Component;
+
+@Component
+// Component tag is must, if we want to create the bean of the Alien class using sprint.
+// Otherwise, ICO container do not know about the existence of the class Alien
 public class Alien {
+
+    public Alien() {
+        System.out.println("Creating bean for Alien class");
+    }
+
     public void code () {
         System.out.println("Coding!!!");
     }
--- SpringDemo/src/main/java/com/example/SpringDemo/SpringDemoApplication.java
+++ SpringDemo/src/main/java/com/example/SpringDemo/SpringDemoApplication.java
@@ -2,17 +2,25 @@ package com.example.SpringDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoApplication.class, args);
+		ConfigurableApplicationContext container = SpringApplication.run(SpringDemoApplication.class, args);
 		System.out.println("Welcome to Spring boot demo \n");
 
-		// Creating object of Alien class
-		Alien alien = new Alien();
-		alien.code();
+		// Creating object of Alien class using java constructor
+		// Alien alien1 = new Alien();
+		// alien1.code();
+
+		// Creating object/bean of Alien class using IOC container (Inversion of control).
+		// ICO Container generally contains beans of all class. It will create, maintain and clear the beans.
+		// We do not need to worry about beans/objects, sprint will do this
+		Alien alien2 = container.getBean(Alien.class);
+		alien2.code();
+		
 	}
 
 }
